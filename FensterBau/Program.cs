1b1fe9f6387562c9a945d3ef780740e16a2c9676
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-12-13 12:14:20 +0100

    Fenster Beispiel

---
+++ FensterBau/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Fensterbau-Verwaltungssoftware");
+
+Console.WriteLine("Wieviele Fenster? ");
+int anzahl = Convert.ToInt32(Console.ReadLine());
+Fenster[] fensterliste = new Fenster[anzahl];
+
+for (int zaehler = 0; zaehler < anzahl; zaehler++)
+{
+    Fenster f = new Fenster();
+
+    f.id = zaehler + 1;
+
+    Console.WriteLine("Zimmer: ");
+    f.name = Console.ReadLine();
+
+    Console.WriteLine("Breite: ");
+    f.breite = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Höhe: ");
+    f.hoehe = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Rahmenfarbe: ");
+    f.rahmenfarbe = Console.ReadLine();
+
+    Console.WriteLine("Mehrfachverglasung (J/N): ");
+    string eingabe = Console.ReadLine();
+
+    if (eingabe.ToUpper() == "J")
+    {
+        f.mehrfachverglasung = true;
+    }
+    else
+    {
+        f.mehrfachverglasung = false;
+    }
+
+    Console.WriteLine("Fensterstärke: ");
+    f.staerke = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Material: ");
+    f.material = Console.ReadLine();
+
+    fensterliste[zaehler] = f;
+}
+
+for (int i = 0; i < fensterliste.Length; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine("ID: " + fensterliste[i].id);
+    Console.WriteLine("Name des Zimmers: " + fensterliste[i].name);
+    Console.WriteLine("Breite: " + fensterliste[i].breite);
+    Console.WriteLine("Höhe: " + fensterliste[i].hoehe);
+    Console.WriteLine("Rahmenfarbe: " + fensterliste[i].rahmenfarbe);
+    Console.WriteLine("Mehrfachverglasung: " + fensterliste[i].mehrfachverglasung);
+    Console.WriteLine("Fensterstärke: " + fensterliste[i].staerke);
+    Console.WriteLine("Material: " + fensterliste[i].material);
+    Console.WriteLine("----------------------------------------------------\n");
+}
+
+
+
+//// Exkurs Arrays
+//// Beispiel erfassen von Temperatur jede Stunde
+
+//double temp1 = 20.3;
+//double temp2 = 20.7;
+//double[] temperaturliste = new double[24];
+//temperaturliste[4] = 23.4;
+
+class Fenster
+{
+    public int id;
+    public string name;
+    public double breite;
+    public double hoehe;
+    public double staerke;
+    public bool mehrfachverglasung;
+    public string rahmenfarbe = "weiß"; //weiß ist Standard jetzt
+    public string material;
+
+    // Enumeration, Aufzählung
+    // "2-fach Verglasung"
+    // zum späteren Zeitpunkt
+
+}
--- Klassen/Program.cs
+++ Klassen/Program.cs
@@ -1,77 +1,89 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-        Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
-        // Objektorientierte Programmierung (OOP)
-        // C -> C++, C#, Java
-        // Pascal -> Delphi
-        // Basic -> VisualBasic
-        // Python
+// Objektorientierte Programmierung (OOP)
+// C -> C++, C#, Java
+// Pascal -> Delphi
+// Basic -> VisualBasic
+// Python
 
-        // Funktionelle Programmierung
-        // Haskel, F#
+// Funktionelle Programmierung
+// Haskel, F#
 
-        //Datenstrukturen
-        // Ganzzahlen
-        int meinGanzzahlwert = 10; // 4Byte -> -2,17 bis 2,17 Mrd. 
+//Datenstrukturen
+// Ganzzahlen
+int meinGanzzahlwert = 10; // 4Byte -> -2,17 bis 2,17 Mrd. 
 
-        // Gleitkommazahlen
-        double Gleitkommazahl = 10.0;
+// Gleitkommazahlen
+double Gleitkommazahl = 10.0;
 
-        string Zeichenkette = "Hallo FIU 21/2";
+string Zeichenkette = "Hallo FIU 21/2";
 
-        bool WahrOderFalsch = true;
+bool WahrOderFalsch = true;
 
-        int Groesse = 5;
-        int[] GanzzahlContainer = new int[Groesse]; // Array
-        GanzzahlContainer[0] = 123;
-        GanzzahlContainer[1] = 42;
-        GanzzahlContainer[2] = 0;
-        GanzzahlContainer[3] = 8;
-        GanzzahlContainer[4] = 15;
+int Groesse = 5;
+int[] GanzzahlContainer = new int[Groesse]; // Array
+GanzzahlContainer[0] = 123;
+GanzzahlContainer[1] = 42;
+GanzzahlContainer[2] = 0;
+GanzzahlContainer[3] = 8;
+GanzzahlContainer[4] = 15;
 
-        /*
-         y  ^
-            |
-            |
-            |
-         10 |
-            |
-            |
-            |
-            |
-         5  |
-            |
-            |
-            |    P
-            |
-            +-------------------------------------------------------->
-                 5     10    15    20 ...                            x
+/*
+ y  ^
+    |
+    |
+    |
+ 10 |
+    |
+    |
+    |
+    |
+ 5  |          P2
+    |
+    |
+    |    P1
+    |
+    +-------------------------------------------------------->
+         5     10    15    20 ...                            x
 
-        P(5|2)
+P(5|2)
 
-         */
+ */
 
-        double x = 5.0; // 5 ist ein Literal
-        double y = 2;
-        
-        double[] P = new double[2];
-        P[0] = 5;
-        P[1] = 2;
+double x = 5.0; // 5 ist ein Literal
+double y = 2;
 
-        Punkt P1 = new Punkt(); //P1 ist ein Objekt
-        P1.x = 5.0;
-        P1.y = 2;
+double[] P = new double[2];
+P[0] = 5;
+P[1] = 2;
 
-        Console.WriteLine($"Punkt 1: Wert {P1.x} auf X-Achse");
-        Console.WriteLine("Punkt 1 y-Wert: {0}",P1.y);
+Punkt P1 = new Punkt(); //P1 ist ein Objekt, Instanz, Variable
+P1.x = 5.0;             // Mit dem Punktoperator greift man auf die Member zu
+P1.y = 2;
 
-class Punkt
-{
-    //Member
-    public double x;
+Punkt P2;               // P2 ist jetzt noch 'null'
+P2 = new Punkt();       // P2 hat jetzt ein Punkt-Objekt gespeichert
+P2.x = 10; P2.y = 5;
+
+
+
+Console.WriteLine($"Punkt 1: Wert {P1.x} auf X-Achse");
+Console.WriteLine("Punkt 1 y-Wert: {0}", P1.y);
+Console.WriteLine($"P2 x: {P2.x} y: {P2.y}");
+
+// Eine Klasse beschreibt einen neuen eigenen Datentyp
+// Eine Klasse ist ein Bauplan für Objekte
+class Punkt //Schlüsselwort class erzeugt eine neue Klasse mit angegebenem Bezeichner
+{           
+    //Member, member
+
+    //Felder, fields
+    public double x; //Zugriffsmodifizierer, Sichtbarkeit
     public double y;
+    private double z; //Sichtbarkeit private nur innnerhalb der Klasse (Standard)
 }
 
 
