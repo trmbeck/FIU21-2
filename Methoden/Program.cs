1f3e234bc6296751d0daa1ec43668034ecf18a22
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-12-13 14:32:50 +0100

    PKW Klasse

---
+++ Methoden/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+PKW auto1 = new PKW();
+//auto1.geschwindigkeit = 0;
+auto1.maxGeschwindigkeit = 200;
+auto1.beschleunigen(1000);
+auto1.beschleunigen(-100);
+auto1.beschleunigen(20);
+auto1.beschleunigen(-50);
+//auto1.geschwindigkeit = 1200;
+
+//Console.WriteLine("aktuelle V: " + auto1.geschwindigkeit);
+
+class PKW
+{
+    //UML: Attribute, C#: Felder
+    public string antriebsart;
+    public int leistung;
+    private double geschwindigkeit;
+    public double maxGeschwindigkeit;
+
+    //UML: Operationen, C#: Methoden
+    public void beschleunigen(double a)
+    {
+        geschwindigkeit = geschwindigkeit + a;
+        if (geschwindigkeit > maxGeschwindigkeit)
+        {
+            Console.WriteLine($"Maximalgeschwindigkeit von {maxGeschwindigkeit}km/h erreicht!");
+            geschwindigkeit = maxGeschwindigkeit;
+
+        }
+        Console.WriteLine("V = " + geschwindigkeit);
+    }
+}
