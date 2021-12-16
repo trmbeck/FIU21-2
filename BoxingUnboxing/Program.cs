80a9f36de99be51eea53e1dc2805e07274431384
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-12-16 14:06:04 +0100

    Boxing Unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Boxing und Unboxing");
+Haus Haus = new Haus();
+
+
+Blume Tulpe = new Blume();
+Kraeuter Basilikum = new Kraeuter();
+
+Haus.PflanzenGiessen(Tulpe);
+Haus.PflanzenGiessen(Basilikum);
+
+
+class Haus
+{
+    public void PflanzenGiessen(object o)
+    {
+        if (o.GetType() == typeof(Blume))
+        {
+            Blume b = (Blume)o;//Unboxing
+            b.Giessen();
+        }
+        else
+        {
+            if (o is Kraeuter)
+            {
+                (o as Kraeuter).Giessen();//Unboxing
+            }
+        }
+    }
+}
+
+class Blume
+{
+    public void Giessen()
+    {
+        Console.WriteLine("Blume wird gegossen");
+    }
+}
+
+class Kraeuter
+{
+    public void Giessen()
+    {
+        Console.WriteLine("Kräuter werden gegossen");
+    }
+}
--- Zeichenprogramm/Form1.cs
+++ Zeichenprogramm/Form1.cs
@@ -3,8 +3,13 @@ namespace Zeichenprogramm
     public partial class Form1 : Form
     {
         Circle[] circleList = new Circle[10];
+        Rectangle[] rectangleList = new Rectangle[10];
+        object[] objectList = new object[10];
+
+        int rectangleListIndex = 0;
         int circleListIndex = 0;
-        
+        int objectListIndex = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -17,17 +22,22 @@ namespace Zeichenprogramm
             double radius = Convert.ToDouble(txbRadius.Text);
 
             Circle circle = new Circle(x, y, radius);
+            objectList[objectListIndex] = circle;
+            objectListIndex++;
+
             //evtl. Prüfung, ob Kreis mit gleichen Daten schon vorhanden
-            circleList[circleListIndex] = circle;
-            circleListIndex++;
+            //circleList[circleListIndex] = circle;
+            //circleListIndex++;
             //evtl. Prüfung, ob Index >= Length
 
-            Graphics panelGraphics = panelAusgabe.CreateGraphics();
-            Pen pen = new Pen(Color.Black);
+            //Graphics panelGraphics = panelAusgabe.CreateGraphics();
+            //Pen pen = new Pen(Color.Black);
 
-            System.Drawing.Rectangle rectangle = circle.GetWinFormRectangle();
+            //System.Drawing.Rectangle rectangle = circle.GetWinFormRectangle();
 
-            panelGraphics.DrawEllipse(pen, rectangle);
+            //panelGraphics.DrawEllipse(pen, rectangle);
+
+            DrawCirclesAndRectangles();
 
             for (int i = 0; i < circleListIndex; i++)
             {
@@ -43,11 +53,46 @@ namespace Zeichenprogramm
             double b = Convert.ToDouble(txbRectangleB.Text);
 
             Rectangle rectangle = new Rectangle(x, y, a, b);
+            objectList[objectListIndex] = rectangle;
+            objectListIndex++;
 
+            //rectangleList[rectangleListIndex] = rectangle;
+            //rectangleListIndex++;
+
+            //Graphics panelGraphics = panelAusgabe.CreateGraphics();
+
+            //panelGraphics.DrawRectangle(new Pen(Color.Black), rectangle.GetWinFormsRectangle());
+            DrawCirclesAndRectangles();
+        }
+
+        private void DrawCirclesAndRectangles()
+        {
             Graphics panelGraphics = panelAusgabe.CreateGraphics();
+            panelGraphics.Clear(SystemColors.Control);
 
-            panelGraphics.DrawRectangle(new Pen(Color.Black), rectangle.GetWinFormsRectangle());
+            //for (int i = 0; i < circleListIndex; i++)
+            //{
+            //    panelGraphics.DrawEllipse(new Pen(Color.Black), circleList[i].GetWinFormRectangle());
+            //}
+            //for (int i = 0; i < rectangleListIndex; i++)
+            //{
+            //    panelGraphics.DrawRectangle(new Pen(Color.Red), rectangleList[i].GetWinFormsRectangle());
+            //}
 
+            for (int i = 0; i < objectListIndex; i++)
+            {
+                if (objectList[i] is Circle)
+                {
+                    panelGraphics.DrawEllipse(new Pen(Color.Black), (objectList[i] as Circle).GetWinFormRectangle());
+                }
+                else
+                {
+                    if (objectList[i] is Rectangle)
+                    {
+                        panelGraphics.DrawRectangle(new Pen(Color.Red), (objectList[i] as Rectangle).GetWinFormsRectangle());
+                    }
+                }
+            }
         }
     }
 }
