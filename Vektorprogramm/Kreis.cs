73ce275d2ab7712590239eae3d78e778aeb6f40e
Michael Beck <Michael.Beck@training.lug-ag.de>
2021-12-21 15:30:31 +0100

    Vektorprogramm

---
+++ Vektorprogramm/Kreis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorprogramm
+{
+    internal class Kreis : Vektorgrafik
+    {
+        double mittelpunktX;
+        double mittelpunktY;
+        public override void Verschieben(double deltaX, double deltaY)
+        {
+            posx += deltaX;
+            posy += deltaY;
+            mittelpunktX += deltaX;
+            mittelpunktY += deltaY;
+            Console.WriteLine("Kreis verschoben!");
+        }
+    }
+}
---
+++ Vektorprogramm/Vektorgrafik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorprogramm
+{
+    internal abstract class Vektorgrafik
+    {
+        protected string farbe;
+        protected string füllmuster;
+        protected double posx;
+        protected double posy;
+
+        public abstract void Verschieben(double deltaX, double deltaY);
+
+        public string GetFarbe()
+        {
+            return farbe;
+        }
+
+        public void SetFarbe(string farbe)
+        {
+            this.farbe = farbe;
+        }
+
+        // Getter und Setter für alle Attribute ...
+    }
+}
